f75e1ebbade85235664f0898d216c735e59a4c28
hom6667 <hom6667@likelion.org>
2022-10-31 22:45:02 +0900

    make easy way

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.create( email: 'admin@qut.edu.au', password: 'Qweasd12' )
+user = User.find(1)
+user.add_role :admin
+
+t = User.find(1)
+t.update_attribute :admin, true
